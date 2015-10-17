e6660566258f93b9718e640a0c649202f9d1d577
unknown <methodezela@gmail.com>
2015-10-17 18:44:34 +0200

    various fix

--- public/js/src/landing/lj-landing.js
+++ public/js/src/landing/lj-landing.js
@@ -168,7 +168,13 @@
 	        	ease3d: 'ease-out',
 	        	onSlideComplete: function(){
 
-	        		var city_text = ["Bastille, 21h43","Célina Helloworld", "Morgane Labelle", "Sylvie Larson", "Christiana", "Marion"];
+	        		var city_text = [
+	        			"Bastille, 21h43",
+	        			"Célina Helloworld",
+	        			"Morgane Labelle", 
+	        			"Sylvie Larson"
+	        		];
+	        		
 	        		var slide_index = this.getSlideIndex();
 	       			var $img = $('.kb-slide').eq( slide_index );
 	       			var text = city_text[ slide_index ];
